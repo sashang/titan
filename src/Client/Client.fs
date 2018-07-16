e83227731279988ed0754820da3a0763ee009f80
sashan <sashang@gmail.com>
2018-07-16 15:19:25 +1000

    Use a descriptive variable name.

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -21,21 +21,21 @@ let urlUpdate (result : Client.Pages.PageType option) (model : SinglePageState) 
     match result with
     | None ->
         handleNotFound model
-        
+
     | Some Client.Pages.PageType.Home ->
         handleNotFound model
 
     | Some Client.Pages.PageType.FirstTime ->
-        { model with page = FirstTimeModel({pupil = false; teacher = false}); username = None }, Cmd.none    
-    
+        { model with page = FirstTimeModel({pupil = false; teacher = false}); username = None }, Cmd.none
+
     | Some Client.Pages.PageType.Login ->
         { model with page = LoginModel; username = None }, Cmd.none
 
 let init () : SinglePageState * Cmd<Msg> =
     {page = LoginModel; username = None}, Cmd.none
 
-(*  
-    have a look at the parent-child description at 
+(*
+    have a look at the parent-child description at
     https://elmish.github.io/elmish/parent-child.html to understand how update messages
     propagate from the child to parent. It's more subtle than it appears from surface.
 *)
@@ -52,16 +52,16 @@ let update (msg : Msg) (model : SinglePageState) : SinglePageState * Cmd<Msg> =
     //which means they just want to escape so change the page to the login page
     | FirstTimeMsg Client.FirstTime.Msg.ClickBackground, _ ->
         {page = LoginModel; username = Option.None}, Cmd.none
-    
+
     //Redirect this to the appropriate page
-    | FirstTimeMsg Client.FirstTime.Msg.ClickContinue, { page = FirstTimeModel x; username = _ } ->
-        if x.teacher then
+    | FirstTimeMsg Client.FirstTime.Msg.ClickContinue, { page = FirstTimeModel ft_model; username = _ } ->
+        if ft_model.teacher then
             {page = NewTeacherModel; username = Option.None}, Cmd.none
-        else if x.pupil then
+        else if ft_model.pupil then
             {page = NewPupilModel; username = Option.None}, Cmd.none
         else
             {page = LoginModel; username = Option.None}, Cmd.none
-    
+
     //any other message from the FirstTime page (basically the TogglePupil/ToggleTeacher messages)
     | FirstTimeMsg msg, {page = FirstTimeModel ft_model; username = _}  ->
         let ft_model', _ = FirstTime.update msg ft_model
@@ -71,7 +71,7 @@ let show = function
 | Some x -> string x
 | None -> "Loading..."
 
- 
+
 let view (model : SinglePageState) (dispatch : Msg -> unit) =
     match model.page with
     | LoginModel -> Client.Login.view (LoginMsg >> dispatch)
