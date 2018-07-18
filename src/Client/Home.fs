ec1cdb3fc576ced0e7028332e8acc937ddc1c129
sashan <sashang@gmail.com>
2018-07-18 21:21:25 +1000

    Update the login link.

--- src/Client/Home.fs
+++ src/Client/Home.fs
@@ -5,27 +5,28 @@ open Elmish
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
+open Style
+
 let view () =
-    Container.container [ Container.IsFluid ] 
-        [ Section.section [ ] 
+    Container.container [ Container.IsFluid ]
+        [ Section.section [ ]
             [ Level.level [ ]
                 [ Level.left [ ]
                     [ Level.item [ ]
                         [ Heading.h1 [ Heading.Is3
-                                       Heading.Modifiers [ Modifier.TextColor IsBlack ] ] 
+                                       Heading.Modifiers [ Modifier.TextColor IsBlack ] ]
                                        [ str "The New Kid" ] ] ]
                   Level.right [ ]
-                    [ Level.item [ ]                        
+                    [ Level.item [ ]
                         [ Text.span [ Modifiers [ Modifier.TextSize (Screen.All, TextSize.Is3)
                                                   Modifier.TextColor IsLink ] ] [ a [ Href "#how_it_works" ] [ str "How it Works" ] ] ]
-                      Level.item [ ] 
-                        [ Text.span [ Modifiers [ Modifier.TextSize (Screen.All, TextSize.Is3)
-                                                  Modifier.TextColor IsLink ] ] [ a [ Href "#login" ] [ str "Login" ] ] ] ] ] ]
+                      Level.item [ ]
+                        [ viewLink Pages.Login "Login" ] ] ] ]
           Section.section [ ]
-              [ Hero.hero [ Hero.IsBold                            
+              [ Hero.hero [ Hero.IsBold
                             Hero.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
                     [ Container.container [ Container.IsFluid ]
                         [ Heading.h1 [ Heading.Modifiers [ Modifier.TextColor IsBlack ] ]
                             [ str "Need to grow your classroom?" ]
-                          Heading.h3 [ Heading.Modifiers [ Modifier.TextColor IsBlack ] ] 
+                          Heading.h3 [ Heading.Modifiers [ Modifier.TextColor IsBlack ] ]
                             [ str "Add students to your class virtually" ] ] ] ] ]
