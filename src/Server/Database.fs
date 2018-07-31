c32c723ff0daf54687677d00f8af0bdb04bc7883
sashan <sashang@gmail.com>
2018-07-31 22:40:57 +1000

    Remove application specific crap.
    Remove code that was specific to the SAFE-Bookstore domain example
    program.

--- src/Server/Database.fs
+++ src/Server/Database.fs
@@ -8,19 +8,3 @@ open FSharp.Control.Tasks.ContextInsensitive
 [<RequireQualifiedAccess>]
 type DatabaseType =
     | FileSystem
-
-type IDatabaseFunctions =
-    abstract member LoadWishList : string -> Task<Domain.WishList>
-    abstract member SaveWishList : Domain.WishList -> Task<unit>
-    abstract member GetLastResetTime : unit -> Task<System.DateTime>
-
-/// Start the web server and connect to database
-let getDatabase databaseType startupTime =
-    match databaseType with
-
-    | DatabaseType.FileSystem ->
-        { new IDatabaseFunctions with
-            member __.LoadWishList key = task { return Storage.FileSystem.getWishListFromDB key }
-            member __.SaveWishList wishList = task { return Storage.FileSystem.saveWishListToDB wishList }
-            member __.GetLastResetTime () = task { return startupTime } }
-
