27389ad12c947fad9ee36409b6320eb6b77a2ce4
sashan <sashang@gmail.com>
2019-01-03 20:29:16 +1100

    Tidy up
    Remove unsued type
    Make functions private.

--- src/Server/Database.fs
+++ src/Server/Database.fs
@@ -5,18 +5,17 @@ open Dapper
 open FSharp.Control.Tasks.ContextInsensitive
 open Npgsql
 open System.Collections.Generic
-open System.Data.SqlClient
 open System.Dynamic
 open System.Threading.Tasks
 open ValueDeclarations
 
-let dapper_query<'Result> (query:string) (connection:NpgsqlConnection) =
+let private dapper_query<'Result> (query:string) (connection:NpgsqlConnection) =
     connection.Query<'Result>(query)
     
-let dapper_param_query<'Result> (query:string) (param:obj) (connection:NpgsqlConnection) : 'Result seq =
+let private dapper_param_query<'Result> (query:string) (param:obj) (connection:NpgsqlConnection) : 'Result seq =
     connection.Query<'Result>(query, param)
     
-let dapper_map_param_query<'Result> (query:string) (param : Map<string,_>) (connection:NpgsqlConnection) : 'Result seq =
+let private dapper_map_param_query<'Result> (query:string) (param : Map<string,_>) (connection:NpgsqlConnection) : 'Result seq =
     let expando = ExpandoObject()
     let expandoDictionary = expando :> IDictionary<string,obj>
     for paramValue in param do
@@ -35,8 +34,6 @@ type IDatabase =
 
     abstract member school_from_user_id: string -> Task<Result<Models.School, string>>
 
-
-type private Exists = {t_or_f : string}
 
 type Database() = 
     interface IDatabase with
