de567e4e067484337e880a2b84411a0c0ffe64e1
sashan <sashang@tewtin.com>
2019-01-11 23:30:15 +1100

    Fix build
    Fixed to hopefully work wit hdeploy target and new stuff from a more
    recent safe template.

--- build.fsx
+++ build.fsx
@@ -11,9 +11,14 @@ open System
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
+#load @"paket-files/build/CompositionalIT/fshelpers/src/FsHelpers/ArmHelper/ArmHelper.fs"
+open Cit.Helpers.Arm
+open Cit.Helpers.Arm.Parameters
+open Microsoft.Azure.Management.ResourceManager.Fluent.Core
 
 let serverPath = Path.getFullName "./src/Server"
 let clientPath = Path.getFullName "./src/Client"
+let clientDeployPath = Path.combine clientPath "deploy"
 let deployDir = Path.getFullName "./deploy"
 
 let platformTool tool winTool =
@@ -53,7 +58,9 @@ let openBrowser url =
     |> ignore
 
 Target.create "Clean" (fun _ ->
-    Shell.cleanDirs [deployDir]
+    [ deployDir
+      clientDeployPath ]
+    |> Shell.cleanDirs
 )
 
 Target.create "InstallClient" (fun _ ->
@@ -62,17 +69,12 @@ Target.create "InstallClient" (fun _ ->
     printfn "Yarn version:"
     runTool yarnTool "--version" __SOURCE_DIRECTORY__
     runTool yarnTool "install --frozen-lockfile" __SOURCE_DIRECTORY__
-    runTool yarnTool "run css-build" __SOURCE_DIRECTORY__
     runDotNet "restore" clientPath
-)
-
-Target.create "RestoreServer" (fun _ ->
-    runDotNet "restore" serverPath
 )
 
 Target.create "Build" (fun _ ->
     runDotNet "build" serverPath
-    runDotNet "fable webpack-cli -- --config src/Client/webpack.config.js -p" clientPath
+    runTool yarnTool "webpack-cli -p" __SOURCE_DIRECTORY__
 )
 
 Target.create "Run" (fun _ ->
@@ -80,29 +82,114 @@ Target.create "Run" (fun _ ->
         runDotNet "watch run" serverPath
     }
     let client = async {
-        runDotNet "fable webpack-dev-server -- --config src/Client/webpack.config.js" clientPath
+        runTool yarnTool "webpack-dev-server" __SOURCE_DIRECTORY__
     }
     let browser = async {
         do! Async.Sleep 5000
         openBrowser "http://localhost:8080"
     }
 
-    [ server; client; browser ]
+    let vsCodeSession = Environment.hasEnvironVar "vsCodeSession"
+    let safeClientOnly = Environment.hasEnvironVar "safeClientOnly"
+
+    let tasks =
+        [ if not safeClientOnly then yield server
+          yield client
+          if not vsCodeSession then yield browser ]
+
+    tasks
     |> Async.Parallel
     |> Async.RunSynchronously
     |> ignore
 )
 
+Target.create "Bundle" (fun _ ->
+    let serverDir = deployDir
+    let publicDir = Path.combine deployDir "public"
+
+    let publishArgs = sprintf "publish -c Release -o \"%s\"" serverDir
+    runDotNet publishArgs serverPath
+
+    Shell.copyDir publicDir clientDeployPath FileFilter.allFiles
+)
+
+type ArmOutput =
+    { WebAppName : ParameterValue<string>
+      WebAppPassword : ParameterValue<string> }
+let mutable deploymentOutputs : ArmOutput option = None
+
+Target.create "ArmTemplate" (fun _ ->
+    let environment = Environment.environVarOrDefault "environment" (Guid.NewGuid().ToString().ToLower().Split '-' |> Array.head)
+    let armTemplate = @"arm-template.json"
+    let resourceGroupName = "safe-" + environment
+
+    let authCtx =
+        // You can safely replace these with your own subscription and client IDs hard-coded into this script.
+        let subscriptionId = try Environment.environVar "subscriptionId" |> Guid.Parse with _ -> failwith "Invalid Subscription ID. This should be your Azure Subscription ID."
+        let clientId = try Environment.environVar "clientId" |> Guid.Parse with _ -> failwith "Invalid Client ID. This should be the Client ID of a Native application registered in Azure with permission to create resources in your subscription."
+
+        Trace.tracefn "Deploying template '%s' to resource group '%s' in subscription '%O'..." armTemplate resourceGroupName subscriptionId
+        subscriptionId
+        |> authenticateDevice Trace.trace { ClientId = clientId; TenantId = None }
+        |> Async.RunSynchronously
+
+    let deployment =
+        let location = Environment.environVarOrDefault "location" Region.EuropeWest.Name
+        let pricingTier = Environment.environVarOrDefault "pricingTier" "F1"
+        { DeploymentName = "SAFE-template-deploy"
+          ResourceGroup = New(resourceGroupName, Region.Create location)
+          ArmTemplate = IO.File.ReadAllText armTemplate
+          Parameters =
+              Simple
+                  [ "environment", ArmString environment
+                    "location", ArmString location
+                    "pricingTier", ArmString pricingTier ]
+          DeploymentMode = Incremental }
+
+    deployment
+    |> deployWithProgress authCtx
+    |> Seq.iter(function
+        | DeploymentInProgress (state, operations) -> Trace.tracefn "State is %s, completed %d operations." state operations
+        | DeploymentError (statusCode, message) -> Trace.traceError <| sprintf "DEPLOYMENT ERROR: %s - '%s'" statusCode message
+        | DeploymentCompleted d -> deploymentOutputs <- d)
+)
+
+open Fake.IO.Globbing.Operators
+open System.Net
+
+// https://github.com/SAFE-Stack/SAFE-template/issues/120
+// https://stackoverflow.com/a/6994391/3232646
+type TimeoutWebClient() =
+    inherit WebClient()
+    override this.GetWebRequest uri =
+        let request = base.GetWebRequest uri
+        request.Timeout <- 30 * 60 * 1000
+        request
+
+Target.create "AppService" (fun _ ->
+    let zipFile = "deploy.zip"
+    IO.File.Delete zipFile
+    Zip.zip deployDir zipFile !!(deployDir + @"\**\**")
+
+    let appName = deploymentOutputs.Value.WebAppName.value
+    let appPassword = deploymentOutputs.Value.WebAppPassword.value
+
+    let destinationUri = sprintf "https://%s.scm.azurewebsites.net/api/zipdeploy" appName
+    let client = new TimeoutWebClient(Credentials = NetworkCredential("$" + appName, appPassword))
+    Trace.tracefn "Uploading %s to %s" zipFile destinationUri
+    client.UploadData(destinationUri, IO.File.ReadAllBytes zipFile) |> ignore)
 
 open Fake.Core.TargetOperators
 
 "Clean"
     ==> "InstallClient"
     ==> "Build"
+    ==> "Bundle"
+    ==> "ArmTemplate"
+    ==> "AppService"
 
 "Clean"
     ==> "InstallClient"
-    ==> "RestoreServer"
     ==> "Run"
 
 Target.runOrDefaultWithArguments "Build"
