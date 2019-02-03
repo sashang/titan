c86625a532eb9365214aa839f8bda3d2c17bd524
sashan <sashang@tewtin.com>
2019-02-03 23:02:41 +1100

    Track the live state in the student live view.

--- src/Client/Root/Dashboard/Student/Class/Class.fs
+++ src/Client/Root/Dashboard/Student/Class/Class.fs
@@ -13,16 +13,22 @@ open Fulma
 open ModifiedFableFetch
 open Thoth.Json
 
+type LiveState =
+    | On
+    | Off
+
+
 type Msg =
-    | GoLive of string
+    | GoLive
     | GetSessionSuccess of OpenTokInfo
     | GetEnroledSchoolsSuccess of School list //unfortunately we need this herer as well to get the tutors emails
     | Failure of exn
-    | StopLive of string
+    | StopLive
 
 type Model =
     { Session : obj option
       School : School 
+      Live : LiveState
       OTI : OpenTokInfo option
       Error : APIError option}
 
@@ -46,38 +52,53 @@ let private get_live_session_id (tutor_email : EmailRequest) = promise {
         return failwith ("Failed to go live: " + msg)
 }
 
-//easier to just copy and past this here for now.
-let private get_enroled_schools () = promise {
-    let request = make_get
-    let decoder = Decode.Auto.generateDecoder<GetAllSchoolsResult>()
-    let! response = Fetch.tryFetchAs "/api/get-enroled-schools" decoder request
-    Browser.console.info "received response from get-enroled-schools"
-    match response with
-    | Ok result ->
-        match result.Error with
-        | Some api_error -> return raise (GetEnroledSchoolsEx api_error)
-        | None ->  return result.Schools
-    | Error e ->
-        return raise (GetEnroledSchoolsEx (APIError.init [APICode.Fetch] [e]))
-}
+let private  nav_item_stop_button (dispatch : Msg -> unit) =
+    Navbar.Item.div [ ]
+        [ Button.button 
+            [ Button.Color IsDanger
+              Button.OnClick (fun e -> dispatch StopLive)  ]
+            [ str "Stop" ] ]
+
+let private classroom_level model dispatch =
+    Level.level [ ] [ 
+        Level.left [ ]
+            [ Level.title [ Common.Modifiers [ Modifier.TextTransform TextTransform.UpperCase
+                                               Modifier.TextSize (Screen.All, TextSize.Is5) ]
+                            Common.Props [ Style [ CSSProp.FontFamily "'Montserrat', sans-serif" ]] ] [ str "classroom" ] ]
+        Level.right [ ] [
+            (match model.Live with
+            | On ->
+                nav_item_stop_button dispatch
+            | Off ->
+                Client.Style.button dispatch GoLive "Go Live!" [ ])
+        ]
+    ]
 
 let init school = 
-    { School = school; Session = None; OTI = None; Error = None},
+    { School = school; Session = None; OTI = None; Error = None; Live = Off},
     Cmd.ofPromise get_live_session_id {Email = school.Email} GetSessionSuccess Failure
 
 let update (model : Model) (msg : Msg) =
     //TODO: map this to the OTI record based on the email
     match model, msg with
-    | {OTI = Some oti; Session = Some session}, GoLive email ->
-        Browser.console.info (sprintf "received JoinLive for tutor %s with session = %s" email oti.SessionId)
+    | {OTI = Some oti; Session = Some session; Live = Off}, GoLive ->
+        Browser.console.info (sprintf "received GoLive for student at school %s with session = %s" model.School.SchoolName oti.SessionId)
         let publisher = OpenTokJSInterop.init_pub "publisher" "640x480"
         OpenTokJSInterop.connect_session_with_pub model.Session.Value publisher oti.Token
         OpenTokJSInterop.add_subscriber session
+        {model with Live = On}, Cmd.none
+
+    | _, GoLive ->
+        Browser.console.error ("Bad state for GoLive message")
         model, Cmd.none
 
-    | {Session = Some session}, StopLive email ->
-        Browser.console.info (sprintf "received StopLive for tutor %s" email)
+    | {Session = Some session; Live = On}, StopLive ->
+        Browser.console.info (sprintf "received StopLive for student at school %s" model.School.SchoolName)
         OpenTokJSInterop.disconnect session
+        {model with Live = Off}, Cmd.none
+
+    | _, StopLive ->
+        Browser.console.error ("Bad state for StopLive message")
         model, Cmd.none
 
     | model, GetSessionSuccess oti ->
@@ -110,6 +131,7 @@ let private video =
     ]
 
 let view (model : Model) (dispatch : Msg -> unit) =
+   [ classroom_level model dispatch
      Box.box' [ Common.Props [ HTMLAttr.Id ""
                                Style [ CSSProp.Height "100%" ] ] ]
-        [ video ]
+        [ video ] ]
