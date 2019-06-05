316ed88daadc5872789973ae329cc64396cd8e29
sashan <sashang@tewtin.com>
2019-06-05 21:40:31 +1000

    Correct variable name.

--- src/Server/TitanOpenTok.fs
+++ src/Server/TitanOpenTok.fs
@@ -23,14 +23,14 @@ type TitanOpenTok(key:int, secret:string) =
             | false ->
                 let! new_session = this.open_tok.CreateSession("", OpenTokCore.MediaMode.RELAYED, OpenTokCore.ArchiveMode.MANUAL)
                 tutor_email_to_session.Add(tutor_email, new_session)
-                let in_one_hour = (DateTime.UtcNow.Add(TimeSpan.FromHours(5.0)).Subtract(new DateTime(1970, 1, 1))).TotalSeconds
-                let token = this.open_tok.GenerateToken(new_session.Id, OpenTokCore.Role.MODERATOR, in_one_hour, "name=test")
+                let in_five_hours = (DateTime.UtcNow.Add(TimeSpan.FromHours(5.0)).Subtract(new DateTime(1970, 1, 1))).TotalSeconds
+                let token = this.open_tok.GenerateToken(new_session.Id, OpenTokCore.Role.MODERATOR, in_five_hours, "name=test")
                 let oti = {SessionId = new_session.Id; Token = token; Key = new_session.ApiKey.ToString() }
                 return Ok oti
             | true ->
                 let session = tutor_email_to_session.[tutor_email]
-                let in_one_hour = (DateTime.UtcNow.Add(TimeSpan.FromHours(5.0)).Subtract(new DateTime(1970, 1, 1))).TotalSeconds
-                let token = this.open_tok.GenerateToken(session.Id, OpenTokCore.Role.MODERATOR, in_one_hour, "name=test")
+                let in_five_hours = (DateTime.UtcNow.Add(TimeSpan.FromHours(5.0)).Subtract(new DateTime(1970, 1, 1))).TotalSeconds
+                let token = this.open_tok.GenerateToken(session.Id, OpenTokCore.Role.MODERATOR, in_five_hours, "name=test")
                 return Ok {SessionId = session.Id; Token = token; Key = session.ApiKey.ToString() }
         }
 
